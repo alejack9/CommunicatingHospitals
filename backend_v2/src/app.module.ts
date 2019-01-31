1154663bf18b65ae8b401a15029d84d6fdcf49a8
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-01-31 17:41:12 +0100

    tests

--- backend_v2/src/app.module.ts
+++ backend_v2/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HospitalsController } from './hospitals/hospitals.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HospitalsController],
   providers: [AppService],
 })
 export class AppModule {}
