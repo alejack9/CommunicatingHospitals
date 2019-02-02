de7f53885e71b951a2114896313e04ac7742f611
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-02-02 11:55:12 +0100

    moved backend_v2 to backend

--- backend_v2/src/hospitals/interfaces/hospital.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { GeoJSOFeature } from './geoJSONFeature';
-import { Preparation } from 'src/preparations/interfaces/preparation.interface';
-
-export interface Hospital extends Document {
-  readonly name: string;
-  readonly coordinate: GeoJSOFeature;
-  readonly preparations: [Preparation];
-}
--- backend_v2/src/preparations/interfaces/preparation.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Preparation extends Document {
-  readonly numberOfPreparations: number;
-  readonly date: Date;
-  readonly type: string;
-}
