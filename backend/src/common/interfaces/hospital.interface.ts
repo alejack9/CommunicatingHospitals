0f27ba9209c62c71e6e1ffd7a403bebb37c0be24
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-02-17 01:06:50 +0100

    added average ranks to hospital interface

--- backend/src/common/interfaces/hospital.interface.ts
+++ backend/src/common/interfaces/hospital.interface.ts
@@ -1,9 +1,11 @@
 import { Document } from 'mongoose';
 import { Preparation } from './preparation.interface';
 import { GeoJSONMultiPoint } from './geoJSONMultiPoint.interface';
+import { Rank } from './rank.interface';
 
 export interface Hospital extends Document {
   readonly name: string;
   readonly coordinate: GeoJSONMultiPoint;
   readonly preparations: [Preparation];
+  readonly averageRanks: [Rank];
 }
