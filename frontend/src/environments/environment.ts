bf9cca38e62775755be174e545e20c989ddc299a
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-02-22 02:11:40 +0100

    nothing interesting here, keep going

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   AUTH0_CLIENTID: 'hafjbtKs0JXZQYr7ctxt8SO4cF20SVf3',
   AUTH0_DOMAIN: 'communicating-hospitals.eu.auth0.com',
   AUTH0_AUDIENCE: 'http://localhost:3000',
-  // AUTH0_REDIRECTURL: 'http://localhost:8100/callback'
-  AUTH0_REDIRECTURL: 'communicatinghospitals://callback'
+  AUTH0_REDIRECTURL: 'http://localhost:8100/callback'
+  // AUTH0_REDIRECTURL: 'communicatinghospitals://callback'
 };
 
 /*
