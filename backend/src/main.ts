2b3f84893c8398e006fe337da5bd6ead87ae686c
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-03-23 20:02:00 +0100

    use compression

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
-
+import * as compression from 'compression';
 // import * as fs from 'fs';
 // // Make an environment object that is module-scoped to get environment variables
 // const environments = dotenv.parse(
@@ -25,6 +25,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+  app.use(compression());
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: !detailedResponses,
