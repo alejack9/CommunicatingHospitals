dae1d5123fc8baee48cb2dbfa663507ff50384d8
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-03-23 19:38:36 +0100

    compressed images

--- frontend/src/app/services/linkedin/mock-data.ts
+++ frontend/src/app/services/linkedin/mock-data.ts
@@ -2,19 +2,19 @@ import { Post } from '../../common/interfaces/post';
 export const POSTS: Post[] = [
   {
     name: 'Mazzoni',
-    photo: '../../../assets/ionic.png',
+    photo: '../../../assets/profile.png',
     date: new Date(),
     content: 'content post'
   },
   {
     name: 'Salesi',
-    photo: '../../../assets/ionic.png',
+    photo: '../../../assets/profile.png',
     date: new Date(),
     content: 'content post'
   },
   {
     name: 'Santa Lucia',
-    photo: '../../../assets/ionic.png',
+    photo: '../../../assets/profile.png',
     date: new Date(),
     content: 'content post'
   }
